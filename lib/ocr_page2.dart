e5b0bb98a419aa58d362c824297d71fc17528f09
sopdsfji <isabell0260@naver.com>
2025-07-14 11:00:19 +0900

    [ｆｉｘ］ ＯＣＲ 핵심 기능 오류 수정

--- lib/ocr_page2.dart
+++ lib/ocr_page2.dart
@@ -12,6 +12,7 @@ class OcrPage2 extends StatefulWidget {
 class _OcrPage2State extends State<OcrPage2> {
   File? _imageFile;
   String _recognizedText = '';
+  bool _isUpsideDown = false;
 
   @override
   void initState() {
@@ -22,11 +23,14 @@ class _OcrPage2State extends State<OcrPage2> {
   Future<void> _loadAssetAndRecognizeText() async {
     try {
       // 1. Load image from asset
-      final byteData = await rootBundle.load('assets/img/Korea+English.png');
+      final byteData = await rootBundle.load('assets/img/upsidedown.png');
 
       // 2. Write to a temporary file
+      // ML Kit의 InputImage.fromFile()은 File 타입을 요구하여
+      // 다음 과정이 필요
+      // 핵심 기능 : byteData를 실제 파일로 변환하기 위해 임시 디렉토리에 저장
       final tempDir = await getTemporaryDirectory();
-      final file = File('${tempDir.path}/Korea+English.png');
+      final file = File('${tempDir.path}/upsidedown.png');
       await file.writeAsBytes(byteData.buffer.asUint8List());
 
       setState(() {
@@ -34,12 +38,28 @@ class _OcrPage2State extends State<OcrPage2> {
       });
 
       // 3. Perform OCR
-      final inputImage = InputImage.fromFile(file);
-      final textRecognizer = TextRecognizer(script: TextRecognitionScript.korean);
+      final inputImage = InputImage.fromFile(file); // OCR 입력 형식
+      // <ML Kit OCR 엔진 객체>
+      final textRecognizer = TextRecognizer(script: TextRecognitionScript.korean); //한국어 텍스트 특화
       final recognizedText = await textRecognizer.processImage(inputImage);
 
+      // 거꾸로 되어 있는지 추정 (방법: 첫 블록의 위치가 이미지 아래쪽에 있는 경우)
+      final blocks = recognizedText.blocks; // 첫번째 텍스트 블록의 좌표값 가져옴
+      if (blocks.isNotEmpty) {
+        final firstBlock = blocks.first;
+        final top = firstBlock.boundingBox?.top ?? 0;  // 해당 값이 클수록 아랫부분에 있는 텍스트
+        final height = firstBlock.boundingBox?.height ?? 0;
+
+        // 화면 아래쪽에서 시작하면 거꾸로일 확률 높음
+        if (top > 500) { // 화면 크기에 따라 조정 필요
+          _isUpsideDown = true;
+        }
+      }
+
       setState(() {
-        _recognizedText = recognizedText.text;
+        _recognizedText = _isUpsideDown
+            ? '※ 거꾸로 되어있음!' // 거꾸로면 안내문
+            : recognizedText.text;  // 인식된 텍스트 저장
       });
 
       textRecognizer.close();
